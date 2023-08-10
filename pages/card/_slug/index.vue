abd9135b3f9cb1b52e7f5860ecd02a24c4380b8f
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2023-08-10 19:12:10 -0400

    feat: create index and card pages, display basic info + links in index

---
+++ pages/card/_slug/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello card page</h1>
+</template>
+  
+<script lang="ts">
+import { fetchFifaCard } from '../../../lib/fifaCardApi'
+
+export default {
+    name: 'CardPage',
+    async asyncData({ params }) {
+        const fifaCard = await fetchFifaCard(params.slug)
+        return { fifaCard }
+    },
+}
+</script>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <Tutorial />
+  <main>
+    <div v-for="card in fifaCards" :key="card.name">
+      <NuxtLink :to="'card/' + card.slug.current">
+        {{ card.name }}
+      </NuxtLink>
+    </div>
+    <CounterTest />
+  </main>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { fetchFifaCards } from '../lib/fifaCardApi'
 
-export default Vue.extend({
+import CounterTest from '../components/Counter.vue'
+
+export default {
   name: 'IndexPage',
-})
+  components: {
+    CounterTest
+  },
+  async asyncData() {
+    const fifaCards = await fetchFifaCards()
+    return { fifaCards }
+  }
+}
 </script>
