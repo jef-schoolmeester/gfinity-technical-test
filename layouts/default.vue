94ce3977b4637322d339bf27daf6eb53f656d86c
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2023-08-11 09:11:48 -0400

    feat: create navbar

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <nav class="bg-zinc-950 flex justify-center sticky top-0">
+            <div class="flex w-full justify-between items-center lg:max-w-5xl">
+                <div class="h-16 w-16 flex justify-center items-center bg-zinc-900">
+                    <img src="/menu.svg" />
+                </div>
+                <img src="/RS-logo.png" class="max-h-5 mr-4"/>
+            </div>
+        </nav>
+        <main>
+            <Nuxt />
+        </main>
+    </div>
+</template>
