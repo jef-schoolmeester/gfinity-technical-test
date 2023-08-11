6b15c33dddb07e0b5c307ef3e15c3e812dc5f553
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2023-08-11 15:43:56 -0400

    feat: add layout & navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <nav class="bg-zinc-950 flex justify-center sticky top-0">
+    <div class="flex flex-col">
+        <nav class="bg-zinc-950 flex justify-center fixed w-full shadow-md shadow-zinc-950">
             <div class="flex w-full justify-between items-center lg:max-w-5xl">
                 <div class="h-16 w-16 flex justify-center items-center bg-zinc-900">
                     <img src="/menu.svg" />
@@ -8,7 +8,7 @@
                 <img src="/RS-logo.png" class="max-h-5 mr-4"/>
             </div>
         </nav>
-        <main>
+        <main class="pt-16 text-white w-full flex justify-center lg:min-h-screen">
             <Nuxt />
         </main>
     </div>
